53c480c0878bbbfebe8db39e731448a08fc78f9a
Sergey Mamyan <sergey.mamyan@gmail.com>
2016-05-19 07:30:00 +0400

    Moved to Typescript 1.8

---
+++ compile.js
@@ -0,0 +1,23 @@
+var exec = require('child_process').execSync;
+var fs = require('fs');
+//var out = '/Users/Sergey/Work/BB/ecmal/elp/out/compiler/index.js';
+var out = '/Users/Sergey/Work/GH/ecmal/compiler/src';
+exec('jake',{
+    cwd     : './typescript',
+    stdio   : 'inherit',
+    env     : process.env
+});
+
+var jsBody = fs.readFileSync('./typescript/built/local/typescript.js','utf8');
+var dtsBody = fs.readFileSync('./typescript/built/local/typescript.d.ts','utf8');
+var jsContent = [
+    'system.register("compiler/index", [], function(system,module) {',
+    jsBody.split('\n').map(function (l){return '  '+l}).join('\n'),
+    '  module.export("TypeScript", ts);',
+    '  module.export("default",ts);',
+    '  return {setters:[],execute:function(){}}',
+    '});'
+].join('\n');
+var dtsContent = dtsBody.replace('export = ts;','export default ts;');
+fs.writeFileSync(out+'/index.js',jsContent);
+fs.writeFileSync(out+'/index.d.ts',dtsContent);
